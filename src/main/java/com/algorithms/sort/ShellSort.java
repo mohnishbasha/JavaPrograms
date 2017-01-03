8f91abf6004b27f7775d005b51f14ed828e4be09
Mohinish <your.email@sap.com>
2017-01-02 19:05:33 -0800

    Updating ShellSort

--- src/main/java/com/algorithms/sort/ShellSort.java
+++ src/main/java/com/algorithms/sort/ShellSort.java
@@ -1,5 +1,19 @@
 package com.algorithms.sort;
 
+/*
+
+G4G:
+http://quiz.geeksforgeeks.org/shellsort/
+
+ShellSort
+ShellSort is mainly a variation of Insertion Sort. In insertion sort, we move elements only one position ahead. When an
+ element has to be moved far ahead, many movements are involved. The idea of shellSort is to allow exchange of far
+ items. In shellSort, we make the array h-sorted for a large value of h. We keep reducing the value of h until it
+ becomes 1. An array is said to be h-sorted if all sublists of every h’th element is sorted.
+
+
+ */
+
 /* O (n log^2 n ) - worst case */
 
 public class ShellSort {
@@ -67,6 +81,42 @@ public class ShellSort {
 	    arr.shellSort();
 	   // arr.display();
 	  }
+
+
+
+        /* function to sort arr using shellSort */
+        public int geeks4geeksShellSort(int arr[])
+        {
+            int n = arr.length;
+
+            // Start with a big gap, then reduce the gap
+            for (int gap = n/2; gap > 0; gap /= 2)
+            {
+                // Do a gapped insertion sort for this gap size.
+                // The first gap elements a[0..gap-1] are already
+                // in gapped order keep adding one more element
+                // until the entire array is gap sorted
+                for (int i = gap; i < n; i += 1)
+                {
+                    // add a[i] to the elements that have been gap
+                    // sorted save a[i] in temp and make a hole at
+                    // position i
+                    int temp = arr[i];
+
+                    // shift earlier gap-sorted elements up until
+                    // the correct location for a[i] is found
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                        arr[j] = arr[j - gap];
+
+                    // put temp (the original a[i]) in its correct
+                    // location
+                    arr[j] = temp;
+                }
+            }
+            return 0;
+        }
+
 	}
 
 /*
